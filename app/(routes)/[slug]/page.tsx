a357b209e6e92b33981c3a8c28d14e48ae124466
Rasel Hossen <raselhossen052@gmail.com>
2024-06-03 22:34:42 +0600

    single-product-page-complete

---
+++ app/(routes)/[slug]/page.tsx
@@ -0,0 +1,65 @@
+import Add from '@/components/Add';
+import CustomizeProducts from '@/components/CustomizeProducts';
+import ProductImages from '@/components/ProductImages';
+import { Suspense } from 'react';
+
+export default function SinglePage() {
+  return (
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+      {/* IMG */}
+      <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
+        <ProductImages />
+      </div>
+      {/* TEXTS */}
+      <div className='w-full lg:w-1/2 flex flex-col gap-6'>
+        <h1 className='text-4xl font-medium'>Product Name</h1>
+        <p className='text-gray-500'>Description</p>
+        <div className='h-[2px] bg-gray-100' />
+        <h2 className='font-medium text-2xl'>Price</h2>
+        {/* {product.price?.price === product.price?.discountedPrice ? (
+        ) : (
+          <div className='flex items-center gap-4'>
+            <h3 className='text-xl text-gray-500 line-through'>
+              ${product.price?.price}
+            </h3>
+            <h2 className='font-medium text-2xl'>
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )} */}
+        <div className='h-[2px] bg-gray-100' />
+        <CustomizeProducts />
+        <Add/>
+        {/* {product.variants && product.productOptions ? (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        ) : (
+          <Add
+            productId={product._id!}
+            variantId='00000000-0000-0000-0000-000000000000'
+            stockNumber={product.stock?.quantity || 0}
+          />
+        )} */}
+        <div className='h-[2px] bg-gray-100' />
+        {/* {product.additionalInfoSections?.map((section: any) => (
+          <div
+            className='text-sm'
+            key={section.title}
+          >
+            <h4 className='font-medium mb-4'>{section.title}</h4>
+            <p>{section.description}</p>
+          </div>
+        ))} */}
+        <div className='h-[2px] bg-gray-100' />
+        {/* REVIEWS */}
+        <h1 className='text-2xl'>User Reviews</h1>
+        {/* <Suspense fallback='Loading...'>
+          <Reviews productId={product._id!} />
+        </Suspense> */}
+      </div>
+    </div>
+  );
+}
