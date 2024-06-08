0cf877c49a6eeb80a4bbf419b06c32f3ca955b2d
Rasel Hossen <raselhossen052@gmail.com>
2024-06-08 16:54:06 +0600

    fix-api-route

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -43,7 +43,9 @@ export default async function HomePage() {
         <h1 className='text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12'>
           Categories
         </h1>
-        <CategoryList />
+        <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense>
       </div>
       <div className='mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
         <h1 className='text-2xl'>New Products</h1>
